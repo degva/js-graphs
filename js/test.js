82ec81efab8eed042096b3a3abbb395d20ff6253
Diego Vargas <degva.29@gmail.com>
2015-01-19 20:19:03 -0500

    In progres

--- js/test.js
+++ js/test.js
@@ -1,82 +1,34 @@
+function Line(data, color) {
+	this.nodes = data;
+	this.color = color;
+	/*
+	this.maxVal = ...;
+	this.minVal = ...;
+	*/
+}
+
+Line.prototype = {
+	constructor = Line,
+	addVal: function (chunk) {
+		this.nodes.push(chunk);
+	}
+};
+
 $(document).ready(function() {
-	var canvas = document.getElementById('plotCanvas');
-	var context = canvas.getContext('2d');
-
-	context.translate(0.5,0.5);
-
-	addGrid(context, canvas.height, canvas.width, 10, 10);
 
 });
 
-function addGrid(context, cH, cW, nX, nY) {
-
-	var uniHeight = cH / nY;
-	var uniWidth = cW / nX;
-
-	context.beginPath();
-
-	for ( var i = 1; i < nX; i++) {
-		context.moveTo(uniWidth * i, 0);
-		context.lineTo(uniWidth * i, cH);
-	}
-
-	for ( var i = 1; i < nY; i++) {
-		context.moveTo(0,						 uniHeight * i);
-		context.lineTo(cW, uniHeight * i);
-	}
-
-	context.strokeStyle = '#aaa';
-	context.stroke();
-	
-}
-
-function init() {
-	updateCanvas();
-}
-
-function updateCanvas() {
-	var canvas = document.getElementById('canvas');
-	var context = canvas.getContext('2d');
-
-	var width = canvas.width;
-	var height = canvas.height;
-
-	var data = [1.4,1.5,1.4,2,1,4,2,3,1,2,3,6,4,7,6,8,6,9];
-
-	/*
-	var circleSize = 10;
-	var gaps = circleSize + 10;
-	*/
-
-	var i = 0;
-	for (d in data) {
-		console.log('Working with: ' + d);
-		if (i != data.length) {
-			context.beginPath();
-
-			x1 = (width / data.length)*(i+0.5);
-			y1 = height - (height / Math.max.apply(Math, data))*(data[i]-0.5);
-
-			x2 = (width / data.length)*(i+1.5);
-			y2 = height - (height / Math.max.apply(Math, data))*(data[i+1]-0.5);
-
-			context.moveTo(x1, y1);
-			context.lineTo(x2, y2);
-			context.lineJoin = 'round';
-			context.lineWidth = 5;
-			context.strokeStyle = '#43A9D1';
-			context.stroke();
-			i++;
-		}
-	}
-	/*
-	for (d in data) {
-		context.fillStyle = aColors[parseInt(Math.random()*aColorsLength)];
-		context.beginPath();
-		context.arc((width / data.length)*(i+0.5), height- (height / Math.max.apply( Math, data) )*(data[i] - 0.5) ,circleSize, 0, Math.PI*2, true);
-		context.closePath();
-		context.fill();
-		i++;
-	}
-	*/
-}
+/*
+ * function Person(var1, var2, var3) {
+ *	this.prop = var1;
+ *	this.prop2 = var2
+ *	...
+ *	this.prop3 = ["asdf","asdf"];
+ * }
+ *
+ * Person.prototype = {
+ *	constructor: Person,
+ *	func : function () {
+ *		do things...;
+ *	}
+ * };
